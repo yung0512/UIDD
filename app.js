09907646636a9b58e4a98b1f03e1c4b53231987b
yung0512 <jeff8651215@gmail.com>
2021-03-17 23:47:43 +0800

    hw1 dynamic html

---
+++ app.js
@@ -0,0 +1,38 @@
+let flag  = true;
+let flag2 = true;
+const getwidth = function(){
+    const width = $(window).width()
+    return width;
+}
+
+
+$('#heading').click(function(event) {
+    event.preventDefault();
+    if(flag2){
+        $('.main-show').css('left','100%')
+        $('#signup').toggleClass('move')
+        flag2=false;
+    }else{
+        $('.main-show').css('left','0')
+        $('#signup').toggleClass('move')
+        flag2 = !flag2;
+    }
+  
+  });
+$('.menu-icon').click(function(event){
+     const width = getwidth();
+     console.log(width)
+     const listWidth = $('.list').width();
+     const value = (listWidth/width)*100;
+     console.log(value)
+    if(flag){
+        $('.list').css("left",`${100-value}%`)
+        flag = false;
+    }else{
+        $('.list').css("left",'100%')
+        flag = true;
+    }
+    // const value = width/listWidth;
+    // $('.click').css("left",`${100-value}%`)
+    $('.menu-icon').toggleClass('hover')
+})
